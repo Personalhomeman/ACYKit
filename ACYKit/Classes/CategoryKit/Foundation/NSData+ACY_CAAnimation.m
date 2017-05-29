338e3c6843c0cb078a576384119f950249683e4d
Arthur LIU <archmagees+cocoapods@gmail.com>
2017-05-29 14:57:07 +0800

    Fix the memory leak bug in producing a CAKeyFrameAnimation.

--- ACYKit/Classes/CategoryKit/Foundation/NSData+ACY_CAAnimation.m
+++ ACYKit/Classes/CategoryKit/Foundation/NSData+ACY_CAAnimation.m
@@ -14,170 +14,99 @@
 @implementation NSData (ACY_CAAnimation)
 
 - (nullable CAKeyframeAnimation *)acy_GIFAnimationWithContentsRect:(CGRect)contentsRect {
-//    guard let src = CGImageSourceCreateWithURL(url as CFURL, nil) else { return nil }
-//    let frameCount = CGImageSourceGetCount(src)
-//
-    CGImageSourceRef src = CGImageSourceCreateWithData((CFDataRef)self, nil);
-    size_t frameCount = CGImageSourceGetCount(src);
+    // 1.0 Get source image
+    CGImageSourceRef source = CGImageSourceCreateWithData((__bridge CFDataRef)self, NULL);
+    size_t count = CGImageSourceGetCount(source);
     
-//    UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:URL]];
-//    
-//    UIImageOrientation orientation = image.imageOrientation;
-//    
-//    CGFloat width = image.size.width;
-//    CGFloat height = image.size.height;
-//    
-//    CGAffineTransform transform = image.acy_transformByImageOrientation;
-//    
-//    
-//    for (size_t i=0; i<count; i++) {
+    float allTime=0;
     
-//    // Total loop time
-//    var time : Float = 0
-        float time = 0;
-//    
-//    // Arrays
-    //    var framesArray = [AnyObject]()
-    //    var tempTimesArray = [NSNumber]()
-    NSMutableArray *framesArray = [NSMutableArray array];
-    NSMutableArray<NSNumber *> *tempTimesArray = [NSMutableArray array];
+    NSMutableArray * framesArray = [[NSMutableArray alloc]init];
+    NSMutableArray * tempTimeArray = [[NSMutableArray alloc]init];
     
-    //    
-//    // Loop
-//    for i in 0..<frameCount {
-    for (NSUInteger i = 0; i < frameCount; i++) {
+    @autoreleasepool {
         
     
-//        
-//        // Frame default duration
-//        var frameDuration : Float = 0.1;
+    for (size_t i = 0; i < count; i++) {
+        
+        
+        // 2.0 set image with crop rect
+        CGImageRef imageRef = CGImageSourceCreateImageAtIndex(source, i, NULL);
+        
+        CGFloat imageWidth = CGImageGetWidth(imageRef);
+        CGFloat imageHeight = CGImageGetHeight(imageRef);
+        
+        CGFloat x = contentsRect.origin.x * imageWidth;
+        CGFloat y = contentsRect.origin.y * imageHeight;
+        CGFloat w = contentsRect.size.width * imageWidth;
+        CGFloat h = contentsRect.size.height * imageHeight;
+        
+        CGRect rect = CGRectMake(x, y, w, h);
+        
+        
+        CGImageRef cropImage = CGImageCreateWithImageInRect(imageRef, rect);
+        
+        [framesArray addObject:(__bridge_transfer UIImage *)(cropImage)];
+        
+        
+        CGImageRelease(imageRef);
+//        CGImageRelease(cropImage);
+        
+        NSDictionary * info = (__bridge_transfer NSDictionary*)CGImageSourceCopyPropertiesAtIndex(source, i, NULL);
+        
+        NSDictionary * gifProperties = [info objectForKey:(__bridge_transfer NSString *)kCGImagePropertyGIFDictionary];
+        
+        
+        
+        
+        // 2.1 calculate time
         float frameDuration = 0.1f;
-//        
-//        let cfFrameProperties = CGImageSourceCopyPropertiesAtIndex(src, i, nil)
-        CFDictionaryRef cfFrameProperties = CGImageSourceCopyPropertiesAtIndex(src, i, nil);
-//        guard let framePrpoerties = cfFrameProperties as? [String:AnyObject] else {return nil}
         
-        NSDictionary *frameProperties = (__bridge NSDictionary *)cfFrameProperties;
-        
-        
-        CFRelease(cfFrameProperties);
-//        guard let gifProperties = framePrpoerties[kCGImagePropertyGIFDictionary as String] as? [String:AnyObject]
-//        else { return nil }
-        NSDictionary *gifProperties =
-        frameProperties[(NSString*)kCGImagePropertyGIFDictionary];
-        
-//        // Use kCGImagePropertyGIFUnclampedDelayTime or kCGImagePropertyGIFDelayTime
-//        if let delayTimeUnclampedProp = gifProperties[kCGImagePropertyGIFUnclampedDelayTime as String] as? NSNumber {
-//            frameDuration = delayTimeUnclampedProp.floatValue
-//        }
-//        else{
-//            if let delayTimeProp = gifProperties[kCGImagePropertyGIFDelayTime as String] as? NSNumber {
-//                frameDuration = delayTimeProp.floatValue
-//            }
-//        }
-        
-        // Use kCGImagePropertyGIFUnclampedDelayTime or kCGImagePropertyGIFDelayTime
-        NSNumber *delayTimeUnclampedProp =
-        gifProperties[(NSString*)kCGImagePropertyGIFUnclampedDelayTime];
+        float delayTimeUnclampedProp = [[gifProperties objectForKey:(__bridge_transfer NSString *)kCGImagePropertyGIFUnclampedDelayTime] floatValue];
         
         if(delayTimeUnclampedProp) {
-            frameDuration = [delayTimeUnclampedProp floatValue];
+            frameDuration = delayTimeUnclampedProp;
         }
         else {
             NSNumber *delayTimeProp =
-            gifProperties[(NSString*)kCGImagePropertyGIFDelayTime];
+            gifProperties[(__bridge_transfer NSString *)kCGImagePropertyGIFDelayTime];
             
             if(delayTimeProp) {
                 frameDuration = [delayTimeProp floatValue];
             }
         }
-//        
-//        // Make sure its not too small
-//        if frameDuration < 0.011 {
-//            frameDuration = 0.100;
-//        }
         
-        // Make sure its not too small
         if (frameDuration < 0.011f){
             frameDuration = 0.100f;
         }
         
+        [tempTimeArray addObject:[NSNumber numberWithFloat:frameDuration]];
         
-//        
-//        // Add frame to array of frames
-//        if let frame = CGImageSourceCreateImageAtIndex(src, i, nil) {
-//            tempTimesArray.append(NSNumber(value: frameDuration))
-//            framesArray.append(frame)
-//        }
+        allTime += frameDuration;
         
-        [tempTimesArray addObject:[NSNumber numberWithFloat:frameDuration]];
-//
-        
-        // Release
-        CFRelease(cfFrameProperties);
-        
-        // Add frame to array of frames
-        CGImageRef frame = CGImageSourceCreateImageAtIndex(src, i, nil);
-        [framesArray addObject:(__bridge id)(frame)];
-        
-        CGImageRelease(frame);
-        
-        // Compile total loop time
-        time = time + frameDuration;
-//        // Compile total loop time
-//        time = time + frameDuration
-//    }
     }
-//
-//    var timesArray = [NSNumber]()
-//    var base : Float = 0
-//    for duration in tempTimesArray {
-//        timesArray.append(NSNumber(value: base))
-//        base.add( duration.floatValue / time )
-//    }
     
+    }
+    
+    CFRelease(source);
+    
+    // 3.0 Get real times value
     NSMutableArray *timesArray = [NSMutableArray array];
     
     float base = 0;
-    for (NSNumber* duration in tempTimesArray){
+    for (NSNumber* duration in tempTimeArray){
         
-        // !!!: maybe different
         [timesArray addObject:@(base)];
-        
-        base = base + (duration.floatValue / time);
-        
+        base = base + (duration.floatValue / allTime);
     }
-//    
-//    // From documentation of 'CAKeyframeAnimation':
-//    // the first value in the array must be 0.0 and the last value must be 1.0.
-//    // The array should have one more entry than appears in the values array.
-//    // For example, if there are two values, there should be three key times.
-//    timesArray.append(NSNumber(value: 1.0))
+    
     [timesArray addObject:@(1.0)];
-//    
-//    // Create animation
-//    let animation = CAKeyframeAnimation(keyPath: "contents")
-//    
-//    animation.beginTime = AVCoreAnimationBeginTimeAtZero
-//    animation.duration = CFTimeInterval(time)
-//    animation.repeatCount = Float.greatestFiniteMagnitude;
-//    animation.isRemovedOnCompletion = false
-//    animation.fillMode = kCAFillModeForwards
-//    animation.values = framesArray
-//    animation.keyTimes = timesArray
-//    //animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
-//    animation.calculationMode = kCAAnimationDiscrete
-//    
-//    return animation;
     
-    CFRelease(src);
     
-    // Create animation
-    CAKeyframeAnimation* animation =
-    [CAKeyframeAnimation animationWithKeyPath:@"contents"];
+    // 4.0 Create the animation.
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"contents"];
     
     animation.beginTime = AVCoreAnimationBeginTimeAtZero;
-    animation.duration = time;
+    animation.duration = allTime;
     animation.repeatCount = HUGE_VALF;
     animation.removedOnCompletion = NO;
     animation.fillMode = kCAFillModeForwards;
